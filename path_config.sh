ea1b5407eebce34146376fa4ed4f5004c79cba9a
yangchen <sjtuyangchen@icloud.com>
2021-06-03 09:46:50 +0800

    add config

---
+++ path_config.sh
@@ -0,0 +1,3 @@
+export nnUNet_raw_data_base="/date/yangchen/nnUNet_raw_data_base"
+export nnUNet_preprocessed="/home/yangchen/projects/nnUNet/nnUNet_preprocessed"
+export RESULTS_FOLDER="/date/yangchen/nnUNet_raw_data_base/nnUNet_trained_models"
